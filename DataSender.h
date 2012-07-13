ac61d1c13109525f1828645ca84d88208d5a79b9
Ville Saarinen <villes@paju.oulu.fi>
2012-07-13 15:59:00 +0300

    Added fragmentation support for unreliable messages too.

--- DataSender.h
+++ DataSender.h
@@ -11,7 +11,7 @@
 class DataSender{
 
 public:
-    DataSender(ApplicationProtocol* appProto, uint16_t gameTick) : appProto(appProto), gameTick(gameTick){}
+    DataSender(ApplicationProtocol* appProto, uint16_t gameTick) : appProto(appProto), gameTick(gameTick), maxDatagramSize(1400){}
 
     bool send(bool sendNow, uint8_t* buffer, const Message* msg, const Ptr<Socket> sock, bool forward, bool isClient);
     bool sendTo(bool sendNow, uint8_t* buffer, const Message* msg, const Address& addr, bool forward, bool isClient, const Ptr<Socket> sock = 0);
@@ -26,6 +26,9 @@ private:
     ApplicationProtocol* appProto;
     bool isClient;
     uint16_t gameTick;
+    uint16_t maxDatagramSize;
+
+    int sendAndFragment(Ptr<Socket> socket, uint8_t *buffer, uint16_t size, bool reliable, const Address *const addr = 0);
 
 };
 
@@ -80,7 +83,7 @@ bool DataSender::sendTo(bool sendNow, uint8_t* buffer, const Message* msg, const
                 }
             }
         }else{
-            if(sock->SendTo((uint8_t*)buffer, messageSize, 0, addr) == -1){
+            if(sendAndFragment(sock, buffer, messageSize, msg->getReliable(), &addr) == -1){
                 PRINT_ERROR("Problems with server socket buffer." << std::endl);
                 return false;
             }
@@ -100,7 +103,7 @@ bool DataSender::flushTcpBuffer(bool isClient){
     for(std::map<Ptr<Socket>, std::string>::iterator it = tcpBuffer.begin(); it != tcpBuffer.end(); it++){
 
         if(it->first->Send((uint8_t*)it->second.c_str(), it->second.length(), 0) == -1){
-            PRINT_ERROR("Problems with server socket buffer." << std::endl);
+            PRINT_ERROR("Problems with socket buffer1." << std::endl);
             return false;
         }
         tcpBuffer.erase(it);
@@ -129,8 +132,9 @@ bool DataSender::flushUdpBuffer(Ptr<Socket> sock, bool isClient){
     }else{
 
         for(std::map<Address, std::pair<std::string, bool> >::iterator it = udpBuffer.begin(); it != udpBuffer.end(); it++){
-            if(sock->SendTo((uint8_t*)it->second.first.c_str(), it->second.first.length(), 0, it->first) == -1){
-                PRINT_ERROR("Problems with server socket buffer." << std::endl);
+
+            if(sendAndFragment(sock,(uint8_t*)it->second.first.c_str(), it->second.first.length(), false, &(it->first)) == -1){
+                PRINT_ERROR("Problems with socket buffer2." << std::endl);
                 return false;
             }
             it->second.second = false;
@@ -141,6 +145,31 @@ bool DataSender::flushUdpBuffer(Ptr<Socket> sock, bool isClient){
 
     Simulator::Schedule(Time(MilliSeconds(gameTick)), &DataSender::flushUdpBuffer, this, sock, isClient);
     return true;
+}
+
+int DataSender::sendAndFragment(Ptr<Socket> socket, uint8_t *buffer, uint16_t size, bool reliable, const Address *const addr){
+
+    if(size > maxDatagramSize){
+
+        std::string tempBuffer((char*)buffer, size);
+        uint16_t headerSize;
+        if(appProto == 0)
+            headerSize = 0;
+        else
+            headerSize = appProto->getHeaderSize();
+
+        return ApplicationProtocol::sendFragment(tempBuffer, 0, socket, maxDatagramSize, addr, appProto, reliable, headerSize);
+
+    }else{
+
+        if(addr == 0){
+            return socket->Send(buffer, size, 0);
+        }else{
+            return socket->SendTo(buffer, size, 0, *addr);
+        }
+    }
+
+    return -1;  //we should never get here
 }
 
 #endif // DATASENDER_H
