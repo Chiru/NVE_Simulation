9a2232947f5070a8bf04e719364049d406119e70
Ville Saarinen <villes@paju.oulu.fi>
2013-07-04 16:53:20 +0300

    Added some element generation funcionality that allows the generation of XML structs and values.

--- GUI/XmlSerializer.cpp
+++ GUI/XmlSerializer.cpp
@@ -5,3 +5,47 @@ XmlSerializer::XmlSerializer(QString fileName)
 {
 
 }
+
+
+void XmlSerializer::writeToFile()
+{
+
+
+}
+
+
+QString XmlStruct::getElementString(int intend) const
+{
+    QString result("");
+    XmlElement* elem;
+
+    for(int i = 0; i < intend; i++)
+        result.append("\t");
+
+    result.append("<" + elementName + ">\n");
+
+    foreach(elem, values)
+        result.append(elem->getElementString(intend + 1));
+
+    for(int i = 0; i < intend; i++)
+        result.append("\t");
+
+    result.append("</" + elementName + ">\n");
+
+    return result;
+}
+
+
+QString XmlValue::getElementString(int intend) const
+{
+    QString result("");
+
+    for(int i = 0; i < intend; i++)
+        result.append("\t");
+
+    result.append("<" + elementName + "\"" + value + "\"/\n");
+
+    return result;
+}
+
+
--- GUI/XmlSerializer.h
+++ GUI/XmlSerializer.h
@@ -2,15 +2,53 @@
 #define XMLSERIALIZER_H
 
 #include <QString>
+#include <QList>
+
+
+class XmlElement
+{
+
+public:
+    virtual QString getElementString(int intend = 0) const = 0;   //intend == number of tabulators
+    virtual ~XmlElement(){}
+
+protected:
+    QString elementName;
+
+};
+
+class XmlStruct : public XmlElement
+{
+
+public:
+    QString getElementString(int intend) const;
+
+private:
+    QList<XmlElement*> values;
+};
+
+class XmlValue : public XmlElement
+{
+
+public:
+    QString getElementString(int intend) const;
+
+private:
+    QString value;
+
+};
 
 class XmlSerializer
 {
+
 public:
     XmlSerializer(QString fileName);
-    writeToFile();
+    void writeToFile();
 
 private:
+
     QString fileName;
+    void writeValue();
 
 };
 
